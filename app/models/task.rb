7e423de6d5100198bedb8e20001d9bd4db784129
João Paulo Lethier <jplethier@gmail.com>
2013-10-13 16:57:57 -0300

    creates task model

---
+++ app/models/task.rb
@@ -0,0 +1,6 @@
+class Task < ActiveRecord::Base
+  belongs_to :project
+
+  validates :description, presence: true
+  validates :project,     presence: true
+end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Task do
+  subject { task }
+
+  let(:task) { FactoryGirl.build :task }
+
+  its(:valid?) { should be_true }
+
+  describe 'validations' do
+    it 'requires a description' do
+      expect { task.description = nil }.to change { task.valid? }.from(true).to(false)
+    end
+
+    it 'requires a project' do
+      expect { task.project = nil }.to change { task.valid? }.from(true).to(false)
+    end
+  end
+end
+
+end

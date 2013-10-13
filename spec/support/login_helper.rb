15cf525264947b898588abe7ba0bad86e5b36945
João Paulo Lethier <jplethier@gmail.com>
2013-10-13 16:23:52 -0300

    configure test environment

---
+++ spec/support/login_helper.rb
@@ -0,0 +1,14 @@
+module LoginHelper
+  include Warden::Test::Helpers
+
+  RSpec.configure do |config|
+    config.before(:each) do
+      Warden.test_mode!
+    end
+
+    config.after(:each) do
+      Warden.test_reset!
+    end
+  end
+end
+

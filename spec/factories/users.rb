b46a5e883eb1294df665bd689b0c95aeefac37b2
João Paulo Lethier <jplethier@gmail.com>
2013-10-13 16:31:38 -0300

    user's model tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:email) { |n| "factory_#{n}@mail.com" }
+    password '123qwe'
+    password_confirmation '123qwe'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { user }
+
+  let(:user) { FactoryGirl.build :user }
+
+  its(:valid?) { should be_true }
+
+  describe 'validations:' do
+    describe 'email' do
+      it 'is required' do
+        expect { user.email = nil }.to change { user.valid? }.from(true).to(false)
+      end
+
+      it 'has to be a valid email' do
+        user.email = 'valid@example.com'
+        expect { user.email = 'invalidexample.com' }.to change { user.valid? }.from(true).to(false)
+      end
+
+      it 'is unique' do
+        existing_user = FactoryGirl.create :user, email: 'duplicated@example.com'
+        expect { user.email = existing_user.email }.to change { user.valid? }.from(true).to(false)
+      end
+    end
+
+    describe 'password' do
+      it 'is required' do
+        expect { user.password = nil }.to change { user.valid? }.from(true).to(false)
+      end
+
+      it 'requires a password confirmation' do
+        expect { user.password_confirmation = 'different' }.to change { user.valid? }.from(true).to(false)
+      end
+
+      describe 'ensures that password is at least 6 length' do
+        it { expect { user.password = '123' }.  to change { user.valid? }.from(true).to(false) }
+        it { expect { user.password = '12345' }.to change { user.valid? }.from(true).to(false) }
+      end
+    end
+  end
 end
